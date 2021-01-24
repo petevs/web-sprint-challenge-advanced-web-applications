b5a74a4831f6201eca867f235f625a76f6f19cd0
Pete VS <pete_vs88@me.com>
2021-01-24 12:05:55 -0500

    tests added

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,28 +4,40 @@ import axios from "axios";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { getColors } from '../api/getColors'
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
 
   // fetch your colors data from the server when the component mounts
-  const getColors = () => {
-    axiosWithAuth().get('http://localhost:5000/api/colors')
-      .then(res => {
-        setColorList(res.data)
-      })
-      .catch(err => console.log(err))
-  }
+  // set that data to the colorList state property
+
+  // const getColors = () => {
+  //   axiosWithAuth().get('http://localhost:5000/api/colors')
+  //     .then(res => {
+  //       setColorList(res.data)
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+
+  // const refreshColors = () => getColors().then(res => {
+  //   console.log(res.data)
+  //   setColorList(res.data)
+  // })
+
+  const refreshColors = async () => setColorList(await getColors())
 
   useEffect(() => {
-    getColors()
+    refreshColors()
   },[])
 
-  // set that data to the colorList state property
+  // useEffect(()=> {
+  //   console.log(colorList)
+  // },[colorList])
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={getColors} />
+      <ColorList colors={colorList} updateColors={refreshColors} />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,26 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import { colors } from '../utils/testData'
+import { getColors as mockGetColors } from '../api/getColors'
+import Bubbles from './Bubbles'
 
-test("Fetches data and renders the bubbles", () => {
+jest.mock('../api/getColors')
+
+const colorList = colors
+
+test("Fetches data and renders the bubbles", async () => {
   // Finish this test
+  mockGetColors.mockResolvedValueOnce(colorList)
+  const {rerender} = render(<BubblePage />)
+
+  await waitFor( async () => {
+    screen.debug();
+  })
+
+  const colorName = screen.getByText(/aliceblue/i)
+  const bubblesHeading = screen.getByText(/bubbles/i)
+  expect(colorName)
+  expect(bubblesHeading)
+
 });
